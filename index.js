23df37cfebd31366ba135d7f96a2673071a203c5
Dan Leveille <dan14lev@gmail.com>
2020-08-23 04:24:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import {
   Dimensions,
   Animated,
   TouchableWithoutFeedback,
+  I18nManager,
+  Platform,
 } from 'react-native';
 import PropTypes from 'prop-types';
 import styles from './styles';
@@ -17,12 +19,12 @@ type Props = {
   edgeHitWidth: number,
   toleranceX: number,
   toleranceY: number,
-  menuPosition: 'left' | 'right',
+  menuPosition: 'left' | 'right' | 'start' | 'end',
   onChange: Function,
   onMove: Function,
   onSliding: Function,
-  openMenuOffset: number,
-  hiddenMenuOffset: number,
+  openMenuOffsetPercentage: number,
+  hiddenMenuOffsetPercentage: number,
   disableGestures: Function | bool,
   animationFunction: Function,
   onAnimationComplete: Function,
@@ -58,8 +60,23 @@ function shouldOpenMenu(dx: number): boolean {
   return dx > barrierForward;
 }
 
+// returns `true` is menu is positioned `left` or `start`, false otherwise.
+function isMenuPositionedAtStartOfViewport(menuPosition: string): boolean {
+  return menuPosition === 'left' || menuPosition === 'start';
+}
+
+// return 1 multiplier if menu position is `start|left` AND
+// LTR or `end|right` AND RTL, return -1 otherwise.
+function menuPositionMultiplier(menuPosition) {
+    const start = isMenuPositionedAtStartOfViewport(menuPosition);
+    if ((start && !I18nManager.isRTL) || (!start && I18nManager.isRTL)) {
+      return 1;
+    } else {
+      return -1;
+    }
+} 
+
 export default class SideMenu extends React.Component {
-  onLayoutChange: Function;
   onStartShouldSetResponderCapture: Function;
   onMoveShouldSetPanResponder: Function;
   onPanResponderMove: Function;
@@ -75,16 +92,21 @@ export default class SideMenu extends React.Component {
     this.prevLeft = 0;
     this.isOpen = !!props.isOpen;
 
-    const initialMenuPositionMultiplier = props.menuPosition === 'right' ? -1 : 1;
-    const openOffsetMenuPercentage = props.openMenuOffset / deviceScreen.width;
-    const hiddenMenuOffsetPercentage = props.hiddenMenuOffset / deviceScreen.width;
+    const initialMenuPositionMultiplier = menuPositionMultiplier(props.menuPosition);
+
+    // Orion Fork Change: Calculate the offsets pixels from a percentage value, rather then using prop values.
+    // This essentially reverses the logic which used to calculate percentages from pixesl.
+    const { openMenuOffsetPercentage, hiddenMenuOffsetPercentage } = props;
+
+    const openMenuOffset = (deviceScreen.width / 100) * openMenuOffsetPercentage;
+    const hiddenMenuOffset = (deviceScreen.width / 100) * hiddenMenuOffsetPercentage;
+    
     const left: Animated.Value = new Animated.Value(
       props.isOpen
-        ? props.openMenuOffset * initialMenuPositionMultiplier
-        : props.hiddenMenuOffset,
+        ? openMenuOffset * initialMenuPositionMultiplier
+        : hiddenMenuOffset * initialMenuPositionMultiplier
     );
 
-    this.onLayoutChange = this.onLayoutChange.bind(this);
     this.onStartShouldSetResponderCapture = props.onStartShouldSetResponderCapture.bind(this);
     this.onMoveShouldSetPanResponder = this.handleMoveShouldSetPanResponder.bind(this);
     this.onPanResponderMove = this.handlePanResponderMove.bind(this);
@@ -94,17 +116,17 @@ export default class SideMenu extends React.Component {
     this.state = {
       width: deviceScreen.width,
       height: deviceScreen.height,
-      openOffsetMenuPercentage,
-      openMenuOffset: deviceScreen.width * openOffsetMenuPercentage,
+      openMenuOffsetPercentage,
+      openMenuOffset,
       hiddenMenuOffsetPercentage,
-      hiddenMenuOffset: deviceScreen.width * hiddenMenuOffsetPercentage,
+      hiddenMenuOffset,
       left,
     };
 
     this.state.left.addListener(({value}) => this.props.onSliding(Math.abs((value - this.state.hiddenMenuOffset) / (this.state.openMenuOffset - this.state.hiddenMenuOffset))));
   }
 
-  UNSAFE_componentWillMount(): void {
+  componentWillMount(): void {
     this.responder = PanResponder.create({
       onStartShouldSetResponderCapture: this.onStartShouldSetResponderCapture,
       onMoveShouldSetPanResponder: this.onMoveShouldSetPanResponder,
@@ -114,18 +136,35 @@ export default class SideMenu extends React.Component {
     });
   }
 
-  UNSAFE_componentWillReceiveProps(props: Props): void {
+  componentWillReceiveProps(props: Props): void {
     if (typeof props.isOpen !== 'undefined' && this.isOpen !== props.isOpen && (props.autoClosing || this.isOpen === false)) {
       this.openMenu(props.isOpen);
     }
   }
 
-  onLayoutChange(e: Event) {
-    const { width, height } = e.nativeEvent.layout;
-    const openMenuOffset = width * this.state.openOffsetMenuPercentage;
-    const hiddenMenuOffset = width * this.state.hiddenMenuOffsetPercentage;
-    this.setState({ width, height, openMenuOffset, hiddenMenuOffset });
+  componentDidUpdate(prevProps) {
+    const { layout } = this.props;
+    if ( prevProps.layout.viewportHeight !== layout.viewportHeight || prevProps.layout.viewportWidth !== layout.viewportWidth ) {
+      this.changeOffset({ width: layout.viewportWidth, height: layout.viewportHeight });
+    }  
   }
+
+  changeOffset = ({ width, height }) => {
+    const { openMenuOffsetPercentage, hiddenMenuOffsetPercentage } = this.props;
+    
+    const openMenuOffset = (width / 100) * openMenuOffsetPercentage;
+    const hiddenMenuOffset = (width / 100) * hiddenMenuOffsetPercentage;
+    
+    this.setState({
+      width,
+      height,
+      openMenuOffset,
+      hiddenMenuOffset,
+      openMenuOffsetPercentage,
+      hiddenMenuOffsetPercentage,
+    });
+    this.moveLeft(this.isOpen ? openMenuOffset : hiddenMenuOffset);
+  }	
 
   /**
    * Get content view. This view will be rendered over menu
@@ -142,12 +181,12 @@ export default class SideMenu extends React.Component {
       );
     }
 
-    const { width, height } = this.state;
+    const { width, height, left } = this.state; 
     const ref = sideMenu => (this.sideMenu = sideMenu);
     const style = [
       styles.frontView,
-      { width, height },
-      this.props.animationStyle(this.state.left),
+      { width, height, },
+      this.props.animationStyle(left),
     ];
 
     return (
@@ -159,7 +198,7 @@ export default class SideMenu extends React.Component {
   }
 
   moveLeft(offset: number) {
-    const newOffset = this.menuPositionMultiplier() * offset;
+    const newOffset = menuPositionMultiplier(this.props.menuPosition) * offset;
 
     this.props
       .animationFunction(this.state.left, newOffset)
@@ -168,16 +207,12 @@ export default class SideMenu extends React.Component {
     this.prevLeft = newOffset;
   }
 
-  menuPositionMultiplier(): -1 | 1 {
-    return this.props.menuPosition === 'right' ? -1 : 1;
-  }
-
   handlePanResponderMove(e: Object, gestureState: Object) {
-    if (this.state.left.__getValue() * this.menuPositionMultiplier() >= 0) {
+    if (this.state.left.__getValue() * menuPositionMultiplier(this.props.menuPosition) >= 0) {
       let newLeft = this.prevLeft + gestureState.dx;
 
       if (!this.props.bounceBackOnOverdraw && Math.abs(newLeft) > this.state.openMenuOffset) {
-        newLeft = this.menuPositionMultiplier() * this.state.openMenuOffset;
+        newLeft = menuPositionMultiplier(this.props.menuPosition) * this.state.openMenuOffset;
       }
 
       this.props.onMove(newLeft);
@@ -186,7 +221,7 @@ export default class SideMenu extends React.Component {
   }
 
   handlePanResponderEnd(e: Object, gestureState: Object) {
-    const offsetLeft = this.menuPositionMultiplier() *
+    const offsetLeft = menuPositionMultiplier(this.props.menuPosition) *
       (this.state.left.__getValue() + gestureState.dx);
 
     this.openMenu(shouldOpenMenu(offsetLeft));
@@ -203,11 +238,13 @@ export default class SideMenu extends React.Component {
         return touchMoved;
       }
 
-      const withinEdgeHitWidth = this.props.menuPosition === 'right' ?
-        gestureState.moveX > (deviceScreen.width - this.props.edgeHitWidth) :
+      const start = isMenuPositionedAtStartOfViewport(this.props.menuPosition);
+      // If `right|end` OR `left|start` and RTL then calculate edgeHitWidth using screen width.
+      const withinEdgeHitWidth = (!start || (start && I18nManager.isRTL)) ?
+        gestureState.moveX > (this.state.width - this.props.edgeHitWidth) :
         gestureState.moveX < this.props.edgeHitWidth;
 
-      const swipingToOpen = this.menuPositionMultiplier() * gestureState.dx > 0;
+      const swipingToOpen = menuPositionMultiplier(this.props.menuPosition) * gestureState.dx > 0;
       return withinEdgeHitWidth && touchMoved && swipingToOpen;
     }
 
@@ -233,21 +270,39 @@ export default class SideMenu extends React.Component {
     return !disableGestures;
   }
 
-  render(): React.Element<void, void> {
-    const boundryStyle = this.props.menuPosition === 'right' ?
-      { left: this.state.width - this.state.openMenuOffset } :
-      { right: this.state.width - this.state.openMenuOffset };
+  getBoundryStyleByDirection(): Object {
+    const boundryEdge = this.state.width - this.state.openMenuOffset;
+    const width = this.state.width - boundryEdge;
+    const start = isMenuPositionedAtStartOfViewport(this.props.menuPosition);
+    // If the RTL setting matches the menuPosition prop
+    // value, then return start and end values which are 
+    // responsive to RTL direction for menu boundry.
+    if (start) {
+      return { start: 0, end: boundryEdge, width: width };
+    }
+    else {
+      return { end: 0, start: boundryEdge, width: width }; 
+    }
+  }
 
+  render(): React.Element<void, void> {
+    const boundryStyle = this.getBoundryStyleByDirection();
+    // On native platforms we were having a weird reaction to 
+    // absolutly positioning the menu, so we remove the styling.
+    const menuStyle = Platform.OS !== 'web' ? {} : styles.menu;
     const menu = (
-      <View style={[styles.menu, boundryStyle]}>
+      <View style={[menuStyle, boundryStyle]}>
         {this.props.menu}
       </View>
     );
 
+    // IMPORTANT: `drawer-container` testID is used to identify this
+    // element in HTML with a CSS selector which allows us to fix an
+    // IE 11 bug when running the drawer on web.
     return (
       <View
-        style={styles.container}
-        onLayout={this.onLayoutChange}
+        style={styles.container} 
+        testID={'drawer-container'}
       >
         {menu}
         {this.getContentView()}
@@ -260,13 +315,13 @@ SideMenu.propTypes = {
   edgeHitWidth: PropTypes.number,
   toleranceX: PropTypes.number,
   toleranceY: PropTypes.number,
-  menuPosition: PropTypes.oneOf(['left', 'right']),
+  menuPosition: PropTypes.oneOf(['left', 'right', 'start', 'end']),
   onChange: PropTypes.func,
   onMove: PropTypes.func,
   children: PropTypes.node,
   menu: PropTypes.node,
-  openMenuOffset: PropTypes.number,
-  hiddenMenuOffset: PropTypes.number,
+  openMenuOffsetPercentage: PropTypes.number,
+  hiddenMenuOffsetPercentage: PropTypes.number,
   animationStyle: PropTypes.func,
   disableGestures: PropTypes.oneOfType([PropTypes.func, PropTypes.bool]),
   animationFunction: PropTypes.func,
@@ -275,6 +330,7 @@ SideMenu.propTypes = {
   isOpen: PropTypes.bool,
   bounceBackOnOverdraw: PropTypes.bool,
   autoClosing: PropTypes.bool,
+  layout: PropTypes.object,
 };
 
 SideMenu.defaultProps = {
@@ -283,7 +339,7 @@ SideMenu.defaultProps = {
   edgeHitWidth: 60,
   children: null,
   menu: null,
-  openMenuOffset: deviceScreen.width * (2 / 3),
+  openMenuOffsetPercentage: 66,
   disableGestures: false,
   menuPosition: 'left',
   hiddenMenuOffset: 0,
@@ -299,7 +355,6 @@ SideMenu.defaultProps = {
   animationFunction: (prop, value) => Animated.spring(prop, {
     toValue: value,
     friction: 8,
-    useNativeDriver: true,
   }),
   onAnimationComplete: () => {},
   isOpen: false,
